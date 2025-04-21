70b978c3b0e8b27ea6e5a97526a684e45040a149
Mr.VB <dragonxdgames@gmail.com>
2025-04-21 20:57:58 -0300

    portfolio page

---
+++ src/main/java/br/com/victorbarberino/portfolio/lp/LpController.java
@@ -0,0 +1,31 @@
+package br.com.victorbarberino.portfolio.lp;
+
+import br.com.victorbarberino.portfolio.project.ProjectService;
+import br.com.victorbarberino.portfolio.testimonial.TestimonialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+
+@Controller
+@RequestMapping("/")
+public class LpController {
+    @Autowired
+    private ProjectService projectService;
+    @Autowired
+    private TestimonialService testimonialService;
+
+    @GetMapping
+    public ModelAndView dispatchLandingPageView() {
+        ModelAndView mv = new ModelAndView("index");
+        Integer actualYear = LocalDate.now().getYear();
+
+        mv.addObject("actualYear", actualYear);
+        mv.addObject("projects", projectService.getAllProjects());
+        mv.addObject("testimonials", testimonialService.getAllTestimonials());
+        return mv;
+    }
+}
